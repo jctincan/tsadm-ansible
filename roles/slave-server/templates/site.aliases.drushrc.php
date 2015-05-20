6e322960916e597844c27d40c1e90df6a8a0d975
Jeremias <jeremias@tincan.co.uk>
2015-05-20 20:48:12 -0300

    slave site envs drush aliases

--- roles/slave-server/templates/site.aliases.drushrc.php
+++ roles/slave-server/templates/site.aliases.drushrc.php
@@ -1,19 +1,20 @@
 <?php
 # {{ansible_managed}}
-
-{% for tsadm_env in item.envs %}
+{% for tsadm_env in tsadmdb_siteenv.values() %}
+{% if tsadm_env.site_id == item %}
+# *** {{tsadmdb_site[item].name}}.{{tsadm_env.name}}
 $aliases['{{tsadm_env.name}}'] = array(
-    'root' => '{{tsadm_homedir}}/sites/{{item.site_name}}/{{tsadm_env.name}}/docroot',
-    'uri' => '{{item.site_name}}{{tsadm_env.name}}.tsadm.tincan.co.uk',
-{% if tsadm_env.host_fqdn != inventory_hostname %}
-    'remote-host' => '{{tsadm_env.host_fqdn}}',
-    'remote-user' => 'tsadm',
+    'root' => '{{tsadm_homedir}}/sites/{{tsadmdb_site[item].name}}/{{tsadm_env.name}}/docroot',
+    'uri' => '{{tsadmdb_site[item].name}}{{tsadm_env.name}}.{{tsadm_master_domain}}',
+{% if tsadmdb_host[tsadm_env.host_id].fqdn != inventory_hostname %}
+    'remote-host' => '{{tsadmdb_host[tsadm_env.host_id].fqdn}}',
+    'remote-user' => '{{tsadm_user}}',
     'path-aliases' => array(
         '%dump-dir' => '/var/tmp/',
     ),
 {% endif %}
 );
-
+{% endif %}
 {% endfor %}
 # {{ansible_managed}}
 ?>

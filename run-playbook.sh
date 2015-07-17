28f8154955bfd2a29059a2f6a156471b79a081cc
Jeremias <jeremias@tincan.co.uk>
2015-07-16 21:47:03 -0300

    show ansible playbook (run)time info

--- run-playbook.sh
+++ run-playbook.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
 privdir=`realpath $(dirname $0)/private`
-exec ansible-playbook -s -T 15 -i invlist.py -e "privdir=${privdir}" $@
+exec time ansible-playbook -s -T 15 -i invlist.py -e "privdir=${privdir}" $@

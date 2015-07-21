4cd7c0673deabe3ccb60620d758250bafb4169ad
Jeremias <jeremias@tincan.co.uk>
2015-07-21 18:35:18 -0300

    run ansible-playbook with -v

--- run-playbook.sh
+++ run-playbook.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
 privdir=`realpath $(dirname $0)/private`
-time ansible-playbook -s -T 15 -i invlist.py -e "privdir=${privdir}" $@
+time ansible-playbook -v -s -T 15 -i invlist.py -e "privdir=${privdir}" $@

adedc76fe89358070f3f758b673665e14e0f1805
Jeremias <jeremias@tincan.co.uk>
2015-07-22 17:19:40 -0300

    check private dir is writable

--- run-playbook.sh
+++ run-playbook.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -x
 privdir=`realpath $(dirname $0)/private`
+test -d ${privdir} && test -w ${privdir} || {
+    echo "${privdir}: dir not found or not writable" >&2
+    exit 1
+}
 time ansible-playbook -v -s -T 15 -i invlist.py -e "privdir=${privdir}" $@

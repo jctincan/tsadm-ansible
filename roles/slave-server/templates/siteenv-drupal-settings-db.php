19b52abfa030bb356db8a533a2e1c62375bac5da
Jeremias <jeremias@tincan.co.uk>
2015-07-29 21:06:39 -0300

    site env drupal db settings file

---
+++ roles/slave-server/templates/siteenv-drupal-settings-db.php
@@ -0,0 +1,18 @@
+<?php
+# {{ansible_managed}}
+
+$databases = array (
+  'default' => array (
+    'default' => array (
+      'database' => '{{tsadmdb_siteenv[item].site_slug}}{{tsadmdb_siteenv[item].name}}db',
+      'username' => '{{tsadmdb_siteenv[item].site_slug}}',
+      'password' => "{{ lookup('password', privdir+'/'+inventory_hostname+'.'+tsadmdb_siteenv[item].site_slug+'.my.passwd chars=ascii_letters,digits') }}",
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
+
+# {{ansible_managed}}

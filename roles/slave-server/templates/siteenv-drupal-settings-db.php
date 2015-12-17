f63dc0f6121241d36a5e88fc8139ebb3026b07f8
Jeremias <jeremias@tincan.co.uk>
2015-12-17 16:14:16 -0300

    configurable slave mysql host

--- roles/slave-server/templates/siteenv-drupal-settings-db.php
+++ roles/slave-server/templates/siteenv-drupal-settings-db.php
@@ -7,7 +7,7 @@ $databases = array (
       'database' => '{{tsadmdb_siteenv[item].site_slug}}{{tsadmdb_siteenv[item].name}}db',
       'username' => '{{tsadmdb_siteenv[item].site_slug}}',
       'password' => "{{ lookup('password', privdir+'/'+inventory_hostname+'.'+tsadmdb_siteenv[item].site_slug+'.my.passwd chars=ascii_letters,digits') }}",
-      'host' => 'localhost',
+      'host' => '{{mysql_login_host}}',
       'port' => '',
       'driver' => 'mysql',
       'prefix' => '',

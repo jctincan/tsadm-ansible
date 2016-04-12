775a18b8b7a40179e8f4344abcd16138ef9229c9
Jeremias <jeremias@tincan.co.uk>
2016-04-12 14:44:18 -0300

    accept ssh hostkey

--- site_checkout.sh
+++ site_checkout.sh
@@ -5,6 +5,7 @@ SITE=${2:?'site?'}
 SENV=${3:?'site env?'}
 
 ansible ${SERVER} \
-        -i inventory.py -s \
-        -m git \
-        -a "dest=/home/tsadm/sites/${SITE}/${SENV} repo=ssh://${SITE}@tsadm.tincan.co.uk:28128/~/${SITE}.git key_file=/home/tsadm/.ssh/id_rsa"
+    -s \
+    -i inventory.py \
+    -m git \
+    -a "dest=/home/tsadm/sites/${SITE}/${SENV} repo=ssh://${SITE}@tsadm.tincan.co.uk:28128/~/${SITE}.git key_file=/home/tsadm/.ssh/id_rsa accept_hostkey=yes"

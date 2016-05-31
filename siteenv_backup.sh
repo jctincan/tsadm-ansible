c6e065dfa87ee72a66c202264adb4ed01dd685ec
Jeremias <jeremias@tincan.co.uk>
2016-05-31 16:30:28 -0300

    save backups under archive dir

--- siteenv_backup.sh
+++ siteenv_backup.sh
@@ -6,20 +6,22 @@ SENV=${3:?'site env?'}
 
 source $(dirname ${BASH_SOURCE})/env.bash
 
-### mysqldump lonrestestdb
-### tgz /home/tsadm/sites/lonres/test
-
 tstamp=$(date '+%Y%m%d.%H%M%S')
+archive_dir=${TSADM_HOMEDIR}/archive
 site_dir=${SITES_BASEDIR}/${SITE}
 siteenv_dir=${site_dir}/${SENV}
-siteenv_file=${site_dir}/${SITE}${SENV}-${tstamp}.tgz
+siteenv_file=${archive_dir}/${SITE}${SENV}-${tstamp}.tgz
 dbname="${SITE}$(echo ${RUN_MODE} | tr [:lower:] [:upper:])${SENV}db"
-dbfile=${site_dir}/${dbname}-${tstamp}.sql.gz
+dbfile=${archive_dir}/${dbname}-${tstamp}.sql.gz
+
+ansible ${SERVER} \
+    -s -i inventory.py  -m file \
+    -a "path=${archive_dir} state=directory mode=750 owner=${TSADM_USER}"
 
 ansible ${SERVER} \
     -s -i inventory.py \
     -m mysql_db -a "name=${dbname} state=dump target=${dbfile}"
 
 ansible ${SERVER} \
-    -s -i inventory.py \
-    -m shell -a "ionice -c3 tar -czf ${siteenv_file} --exclude='.git' ${siteenv_dir}/"
+    -s -i inventory.py -m shell \
+    -a "ionice -c3 tar -czf ${siteenv_file} --exclude='.git' ${siteenv_dir}/"

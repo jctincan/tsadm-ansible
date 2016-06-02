841f465c6d75d421b8fbf85c99285959e065d9d1
Jeremias <jeremias@tincan.co.uk>
2016-06-02 12:57:09 -0300

    cleanup when removing siteenv

--- site_remove.sh
+++ site_remove.sh
@@ -2,20 +2,15 @@
 
 SERVER=${1:?'server?'}
 SITE=${2:?'site?'}
-SENV=${3:?'site env?'}
 
 source $(dirname ${BASH_SOURCE})/env.bash
 
-dbsettings=${TSADM_HOMEDIR}/drupal-settings/${SITE}.${SENV}-db.php
-privdir=${TSADM_HOMEDIR}/private/${SITE}.${SENV}
 site_slug="${SITE}$(echo ${RUN_MODE} | tr [:lower:] [:upper:])"
-vhost_f=${ETCDIR}/apache.d/*-${site_slug}${SENV}.conf
 sitedir=${SITES_BASEDIR}/${SITE}
 
-for rmpath in ${dbsettings} ${privdir} ${vhost_f} ${sitedir}; do
-    ansible ${SERVER} -s -i inventory.py -m file
-                      -a "path=${rmpath} state=absent"
-done
+ansible ${SERVER} \
+    -s -i inventory.py -m file \
+    -a "path=${sitedi} state=absent"
 
 ansible ${SERVER} \
     -s -i inventory.py -m user \
--- siteenv_remove.sh
+++ siteenv_remove.sh
@@ -6,12 +6,18 @@ SENV=${3:?'site env?'}
 
 source $(dirname ${BASH_SOURCE})/env.bash
 
-dbname="${SITE}$(echo ${RUN_MODE} | tr [:lower:] [:upper:])${SENV}db"
+site_slug="${SITE}$(echo ${RUN_MODE} | tr [:lower:] [:upper:])"
+dbname="${site_slug}${SENV}db"
+dbsettings=${TSADM_HOMEDIR}/drupal-settings/${SITE}.${SENV}-db.php
+envdir=${SITES_BASEDIR}/${SITE}/${SENV}
+privdir=${TSADM_HOMEDIR}/private/${SITE}.${SENV}
+vhost_f=${ETCDIR}/apache.d/*-${site_slug}${SENV}.conf
 
 ansible ${SERVER} \
     -i inventory.py \
     -m mysql_db -a "name=${dbname} state=absent"
 
-ansible ${SERVER} \
-    -s -i inventory.py \
-    -m file -a "path=${SITES_BASEDIR}/${SITE}/${SENV} state=absent"
+for rmpath in ${dbsettings} ${privdir} ${vhost_f} ${envdir}; do
+    ansible ${SERVER} -s -i inventory.py -m file
+                      -a "path=${rmpath} state=absent"
+done

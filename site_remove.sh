5539eeb563e0e6fde20a0cca68155faba3d630ca
Jeremias <jeremias@tincan.co.uk>
2016-06-02 16:51:15 -0300

    remove site drushrc file

--- site_remove.sh
+++ site_remove.sh
@@ -7,10 +7,11 @@ source $(dirname ${BASH_SOURCE})/env.bash
 
 site_slug="${SITE}$(echo ${RUN_MODE} | tr [:lower:] [:upper:])"
 sitedir=${SITES_BASEDIR}/${SITE}
+drush_f=${TSADM_HOMEDIR}/.drush/${SITE}.aliases.drushrc.php
 
-ansible ${SERVER} \
-    -s -i inventory.py -m file \
-    -a "path=${sitedi} state=absent"
+for rmf in ${sitedir} ${drush_f}; do
+    ansible ${SERVER} -s -i inventory.py -m file -a "path=${rmf} state=absent"
+done
 
 ansible ${SERVER} \
     -s -i inventory.py -m user \

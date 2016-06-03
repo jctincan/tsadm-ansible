caab22293dc58cdd5adf6eebde59801fd72588ed
Jeremias <jeremias@tincan.co.uk>
2016-06-03 16:56:27 -0300

    remove site from master server

--- site_remove.sh
+++ site_remove.sh
@@ -8,6 +8,10 @@ source $(dirname ${BASH_SOURCE})/env.bash
 site_slug="${SITE}$(echo ${RUN_MODE} | tr [:lower:] [:upper:])"
 sitedir=${SITES_BASEDIR}/${SITE}
 drush_f=${TSADM_HOMEDIR}/.drush/${SITE}.aliases.drushrc.php
+repos_basedir=/srv/git/${TSADM_USER}
+tstamp=$(date '+%Y%m%d.%H%M%S')
+repo_archive=${repos_basedir}/archive/${SITE}.git-${tstamp}.tgz
+repodir=${SITES_REPO_BASEDIR}/${SITE}/${SITE}.git
 
 for rmf in ${sitedir} ${drush_f}; do
     ansible ${SERVER} -s -i inventory.py -m file -a "path=${rmf} state=absent"
@@ -20,3 +24,19 @@ ansible ${SERVER} \
 ansible ${SERVER} \
     -s -i inventory.py -m mysql_user \
     -a "name=${site_slug} state=absent"
+
+ansible tsadm.tincan.co.uk \
+    -s -i inventory.py -m user \
+    -a "name=${site_slug} state=absent remove=yes"
+
+ansible tsadm.tincan.co.uk \
+    -s -i inventory.py -m file \
+    -a "path=${repos_basedir}/archive state=directory"
+
+ansible tsadm.tincan.co.uk \
+    -s -i inventory.py -m shell \
+    -a "ionice -c3 tar -czf ${repo_archive} ${repodir}/"
+
+ansible tsadm.tincan.co.uk \
+    -s -i inventory.py -m file \
+    -a "path=${repodir} state=absent"
